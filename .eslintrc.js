3e7206d33039cae5d9e98768a4512f65aea2fb90
Adam Skobodziński <adam@skobo.pl>
2019-02-22 09:10:09 +0100

    eslint, jsbeautifier

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  'extends': 'airbnb-base',
+  'settings': {
+    'import/resolver': {
+      'jspm': {
+        'moduleDirectory': 'jspm_packages'
+      }
+    }
+  },
+  'rules': {
+    'import/no-extraneous-dependencies': 'off',
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'max-len': ['error', 80],
+    'no-use-before-define': 'off',
+    'no-console': 'off',
+    'prefer-arrow-callback': 'off',
+    'prefer-const': 'off',
+    'prefer-rest-params': 'off',
+    'function-paren-newline': ['error', 'consistent']
+  },
+  'globals': {
+    'window': true,
+    'Vue': true,
+    'io': true
+  },
+  'plugins': [
+    'import'
+  ]
+};
